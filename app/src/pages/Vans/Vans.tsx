380476ddbf40432abca420862d56697659ec73df
Tomasz Czerwiński <tomasz.czerwinski.1997@gmail.com>
2024-02-25 17:49:46 +0100

    ✨ add the page van details and update css

--- app/src/pages/Vans/Vans.tsx
+++ app/src/pages/Vans/Vans.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export interface IVan {
   id: string
@@ -18,12 +19,14 @@ export default function Vans() {
 
   const vanElements = vans.map(van => (
     <div key={ van.id } className="van-tile">
-      <img alt={ van.name } src={ van.imageUrl } />
-      <div className="van-info">
-        <h3>{ van.name }</h3>
-        <p>${ van.price }<span>/day</span></p>
-      </div>
-      <i className={ `van-type ${ van.type } selected` }>{ van.type }</i>
+      <Link to={ `/vans/${ van.id }` }>
+        <img alt={ van.name } src={ van.imageUrl } />
+        <div className="van-info">
+          <h3>{ van.name }</h3>
+          <p>${ van.price }<span>/day</span></p>
+        </div>
+        <i className={ `van-type ${ van.type } selected` }>{ van.type }</i>
+      </Link>
     </div>
   ))
 
